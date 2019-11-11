078f1ddd5d6cc984fcf265f7f7713e6746c923ae
Shipra <drshipradyundi@gmail.com>
2019-11-10 16:45:51 -0800

    adding pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
         trainTime = $("#traintime-input").val().trim();
         frequency = $("#frequency-input").val().trim();
         console.log("->", trainName, destination, trainTime, frequency)
-            // Code for "Setting values in the database"
+            // Code for "pushing the values in the database"
         database.ref().push({
             trainName: trainName,
             destination: destination,
@@ -43,51 +43,32 @@ $(document).ready(function() {
 
     });
 
-    // Firebase watcher + initial loader HINT: .on("value")
+    // Firebase watcher + initial loader
     database.ref().on("child_added", function(snapshot) {
 
-        // Log everything that's coming out of snapshot
-        console.log(snapshot.val());
-        console.log(snapshot.val().trainName);
-        console.log(snapshot.val().destination);
-        console.log(snapshot.val().trainTime);
-        console.log(snapshot.val().frequency);
+
 
         // next and minitues away
         // current time
         // var current = moment().format("HH:mm")
-        //  console.log(current)
+
         var trainTimeM = moment(snapshot.val().trainTime, "HH:mm");
 
-        // 8:00    9:50   30    9:50 - 8:00 110 minutes  / 30 90   110 - 90 
+        // calculating minutes away and next train
         var minutesAway = snapshot.val().frequency - (moment().diff(trainTimeM, "minutes") % snapshot.val().frequency)
         var nextTrain = moment().add(minutesAway, "minutes").format("hh:mm")
 
-        console.log("---->", minutesAway, nextTrain);
-        // Change the HTML to reflect
-        //$("#name-display").text(snapshot.val().name);
-        //$("#destination-display").text(snapshot.val().email);
-        //$("#traintime-display").text(snapshot.val().age);
-        //$("#comment-display").text(snapshot.val().comment);
-        /////
+
         $("#train-table > tbody").append(`
-                                <tr class="trains">
-                                    <td scope="col">${snapshot.val().trainName}</td>
-                                    <td scope="col">${snapshot.val().destination}</td>
-                                    <td class="info" time=${snapshot.val().trainTime} freq=${snapshot.val().frequency} scope="col">${snapshot.val().frequency}</td>
-                                    <td class="next" scope="col">${nextTrain}</td>
-                                    <td class="minutes" scope="col">${minutesAway}</td>
-                                </tr>`)
+          <tr class="trains">
+            <td scope="col">${snapshot.val().trainName}</td>
+            <td scope="col">${snapshot.val().destination}</td>
+            <td class="info" time=${snapshot.val().trainTime} freq=${snapshot.val().frequency} scope="col">${snapshot.val().frequency}</td>
+            <td class="next" scope="col">${nextTrain}</td>
+            <td class="minutes" scope="col">${minutesAway}</td>
+         </tr>`)
 
 
-
-        /////
-
-
-
-        // Handle the errors
-    }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
     });
 
     function updateTime() {
